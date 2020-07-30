fa93456e9cf64f278753e0d0b7078415f5331a24
Fred Di Pasqua <freddipassca@gmail.com>
2020-07-30 15:08:06 -0500

    configuración para desarrollo

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,15 +1,25 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require("webpack");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: path.resolve(__dirname, './src/index.js')
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'js/[name].js',
+        publicPath: 'http://localhost:8000/',
+        chunkFilename: 'js/[id].[chunkhash].js'
     },
     resolve: {
         extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true,
+        port: 8000,
+        hot: true,
     },
     module: {
         rules: [
@@ -31,24 +41,27 @@ module.exports = {
             {
                 test: /\.(s*)css$/,
                 use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+                use : {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'assets/images/'
+                    }
+                }
             }
 
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './public/index.html',
+            template: path.resolve(__dirname, './public/index.html'),
             filename: './index.html'
         }),
-
-        new MiniCssExtractPlugin({
-            filename: 'assets/[name].css'
-        })
+        new webpack.HotModuleReplacementPlugin()
     ]
 }
