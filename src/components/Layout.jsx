dee4ec0964144367b0c1ab7586c5816a706810da
Fred Di Pasqua <freddipassca@gmail.com>
2020-08-02 15:57:33 -0500

    funcionando particulas, falta config

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Background3D from './animations/Background3D';
+import ParticlesCustom from './animations/Particles'
 
 const Layout = ({ children }) => (
     <>
         <Header />
+        <ParticlesCustom />
         <Footer />
         {children}
         <Background3D/>
---
+++ src/components/animations/Particles.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+import '../../assets/styles/components/Particles.scss';
+
+const ParticlesCustom = () => (
+    <Particles className="particles" />
+);
+
+export default ParticlesCustom;
