b2012d5f0ec892c313e7923bc6d212cbf7f0bc8e
Fred Di Pasqua <freddipassca@gmail.com>
2020-08-14 18:47:43 -0500

    Ya tengo la función para desvanvecer

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import '../assets/styles/components/Main.scss';
-import { blueviolet } from 'color-name';
 
 const Main = () => {
 
-    // function toScroll () {
-    //     document.onload(() => {
-    //         window.scroll(() => {
-    //             if children.scrollTop() > 0 {
-    //                 b
-    //             }
-
-    //         })
-    //     })
-    // }
+    function toScroll () {
+        let mainView = document.getElementsById("mainUp");
+        
+        document.addEventListener("scroll", function(){
+            console.log(("bien"));
+            // window.scroll(function(){
+                if (document.documentElement.scrollTop > 0){
+                    mainView.style.opacity = "0";
+                    console.log("que tal");
+                }
+                else{
+                    mainView.style.opacity = "1";
+                }
+            // })
+        })
+    }
 
     return (
         <div className="main">
-            <h1 className="name"  >Fred Di Pasqua</h1>
-            <p className="title" > Desarrollador | Creador | Artista</p>
-            <p className="quote" >Aqui se supone debe de ir un quote 
-                                    donde cada vez que abras la página
-                                    cargue uno diferente</p>
-            <p className="author" >Autor del Quote</p>
+            <div id="mainUp" className="mainUp">
+                <h1 className="name"  >Fred Di Pasqua</h1>
+                <p className="title" > Desarrollador | Creador | Artista</p>
+                <p className="quote" >Aqui se supone debe de ir un quote 
+                                        donde cada vez que abras la página
+                                        cargue uno diferente</p>
+                <p className="author" >Autor del Quote</p>
+            </div>
             <div className="bio">
                 <p className="bioP" >Desarrollador Junior en tecnologias como React, bootstrap, sass, mongoDB, docker, etc.
                     Creador de Marca de Daho Industries.
