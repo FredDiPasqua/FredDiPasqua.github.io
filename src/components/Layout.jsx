018603ea2f31d718e38c4a8371c67944fb30067d
Fred Di Pasqua <freddipassca@gmail.com>
2020-08-14 02:49:59 -0500

    trabajando en home

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,26 +2,15 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Background3D from './animations/Background3D';
-<<<<<<< HEAD
 import ParticlesCustom from "./animations/Particles"
-=======
-import ParticlesCustom from './animations/Particles'
->>>>>>> particles
 
 const Layout = ({ children }) => (
     <>
         <Background3D/>
-<<<<<<< HEAD
         <Header />
         {children}
         <Footer />
         <ParticlesCustom />
-=======
-        {children}
-        <ParticlesCustom />
-        <Header />
-        <Footer />
->>>>>>> particles
     </>
 );
 
--- src/components/animations/Particles.jsx
+++ src/components/animations/Particles.jsx
@@ -8,11 +8,7 @@ const ParticlesCustom = () => (
             params={{
                 "particles": {
                     "number": {
-<<<<<<< HEAD
                         "value": 100
-=======
-                        "value": 80
->>>>>>> particles
                     },
                     "size": {
                         "value": 2
